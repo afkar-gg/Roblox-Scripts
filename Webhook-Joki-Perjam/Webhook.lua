c97d6d40a4d622a543324e016865e4619e316c5e
afkar-gg <afkarsyarif45@gmail.com>
2025-06-18 20:43:13 +0700

    Update Webhook.lua

--- Webhook-Joki-Perjam/Webhook.lua
+++ Webhook-Joki-Perjam/Webhook.lua
@@ -72,7 +72,7 @@ local embed = {
         },
         {
             ["name"] = "Info Joki",
-            ["value"] = "Waktu joki dimulai : <t:" ..os.time().. ":f> \nWaktu joki selesai : <t:" ..done_joki.. ":f>"
+            ["value"] = "Waktu joki dimulai : <t:" ..os.time().. ":f> \nWaktu joki selesai : <t:" ..os.time() + 3600 * jam_selesai_joki.. ":f>"
         }
     },
     ["footer"] = {
