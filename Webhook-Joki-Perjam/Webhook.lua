3380394d04b6bd4d782470085ab0ad840076e538
afkar-gg <afkarsyarif45@gmail.com>
2025-06-18 20:34:10 +0700

    Update Webhook.lua

--- Webhook-Joki-Perjam/Webhook.lua
+++ Webhook-Joki-Perjam/Webhook.lua
@@ -5,7 +5,7 @@ local username = LocalPlayer.Name -- Get the player's username
 
 local current_time = os.time()
 local wib = current_time + 25200
-local done_joki = current_time + 3600 * jam_selesai_joki
+local done_joki = wib + 3600 * jam_selesai_joki
 
 
 local new_string = string.sub(no_order, 9) -- Start from the 9th character to get the numbers after "OD000000"
@@ -63,7 +63,7 @@ end
 
 local embed = {
     ["title"] = "JOKI DIMULAI",
-    ["description"] = "Username : ||" ..username.. "||", -- Concatenate "Username : " with the username variable
+    ["description"] = "Username : " .. username.. "", -- Concatenate "Username : " with the username variable
     ["color"] = 65280,
     ["fields"] = {
         {
@@ -72,7 +72,7 @@ local embed = {
         },
         {
             ["name"] = "Info Joki",
-            ["value"] = "Waktu joki dimulai : <t:" ..os.time().. ":f> \nWaktu joki selesai : <t:" ..done_joki.. ":f>"
+            ["value"] = "Waktu joki dimulai : <t:" ..os.time().. ":f> \nWaktu joki selesai : <t:" .. done_joki .. ":f>"
         }
     },
     ["footer"] = {
