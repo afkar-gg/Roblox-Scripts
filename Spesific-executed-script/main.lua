ae4ba3aa48f3c3e286f27fd7d206d89c2372e49f
afkar-gg <afkargantengbetjir@gmail.com>
2025-07-08 02:40:10 +0800

    Create Spesific-executed-script/main.lua

---
+++ Spesific-executed-script/main.lua
@@ -0,0 +1,33 @@
+local TeleportService = game:GetService("TeleportService")
+
+local placeScripts = {
+    [116495829188952] = function()
+        script_key = "TqDokjuYYMVIuPOBmYZKjpWCrtnGRmiU"
+        (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))()
+    end,
+
+    [126884695634066] = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+    end,
+
+    [70876832253163] = function()
+        script_key = "TqDokjuYYMVIuPOBmYZKjpWCrtnGRmiU"
+        (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))()
+
+        -- ⏳ Wait 2 minutes
+        task.delay(240, function()
+            -- 🚪 Teleport player to Dead Rails
+            local player = game:GetService("Players").LocalPlayer
+            if player then
+                TeleportService:Teleport(116495829188952, player)
+            end
+        end)
+    end,
+}
+
+local runScript = placeScripts[game.PlaceId]
+if runScript then
+    runScript()
+else
+    return
+end
