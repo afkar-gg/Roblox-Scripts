056e49aa2130ab2ca71b5fc736f676782221ae01
afkar-gg <afkargantengbetjir@gmail.com>
2025-07-08 01:46:12 +0700

    Update main.lua

--- Spesific-executed-script/main.lua
+++ Spesific-executed-script/main.lua
@@ -3,11 +3,12 @@ local TeleportService = game:GetService("TeleportService")
 local placeScripts = {
     [116495829188952] = function()
         script_key = "TqDokjuYYMVIuPOBmYZKjpWCrtnGRmiU"
-        (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))()
+        (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))();
     end,
 
     [126884695634066] = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))();
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/Roblox-Scripts/refs/heads/main/(Remastered)-Roblox-Joki-Proxy/main.lua"))();
     end,
 
     [70876832253163] = function()
