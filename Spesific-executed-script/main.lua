7f1b60a234028941c6c544aa685de062e0563498
afkar-gg <afkargantengbetjir@gmail.com>
2025-07-08 10:16:08 +0800

    Update main.lua

--- Spesific-executed-script/main.lua
+++ Spesific-executed-script/main.lua
@@ -1,34 +1,35 @@
-local tS = game:GetService(string.reverse("ecivreStropeleT"))
+local TeleportServicegame:
+GetService("TeleportService")
 
-local __id2f = {
-    [116495829188952] = function()
-        _G["__k_"..math.random()] = "TqDokjuYYMVIuPOBmYZKjpWCrtnGRmiU"
-        (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))()
-    end,
+local placeScripts = {
+[116495829188952] = function()
+script_key = "TqDokjuYYMVIuPOBmYZKjpWCrtnGRmiU"
+(loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))()
+end,
 
-    [126884695634066] = function()
-        local url = table.concat({
-            "https://raw.githubusercontent.com",
-            "/AhmadV99/Speed-Hub-X/main/",
-            "Speed%20Hub%20X.lua"
-        })
-        loadstring(game:HttpGet(url, true))()
-    end,
+[126884695634066] = function()  
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()  
+end,  
 
-    [70876832253163] = function()
-        _G["__k_"..tick()] = "TqDokjuYYMVIuPOBmYZKjpWCrtnGRmiU"
-        (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))()
+[70876832253163] = function()  
+    script_key = "TqDokjuYYMVIuPOBmYZKjpWCrtnGRmiU"  
+    (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))()  
 
-        task.delay(240, function()
-            local plr = game:GetService("Players").LocalPlayer
-            if plr then
-                tS:Teleport(116495829188952, plr)
-            end
-        end)
-    end,
+    -- ⏳ Wait 2 minutes  
+    task.delay(240, function()  
+        -- 🚪 Teleport player to Dead Rails  
+        local player = game:GetService("Players").LocalPlayer  
+        if player then  
+            TeleportService:Teleport(116495829188952, player)  
+        end  
+    end)  
+end,
+
 }
 
-local __pid = game.PlaceId
-if __id2f[__pid] then
-    (function(f) f() end)(__id2f[__pid])
+local runScript = placeScripts[game.PlaceId]
+if runScript then
+runScript()
+else
+return
 end
