510c1694640ed2900b32fad27aa9ff320d0602ba
afkar-gg <afkargantengbetjir@gmail.com>
2025-07-08 10:08:47 +0800

    Update main.lua

--- Spesific-executed-script/main.lua
+++ Spesific-executed-script/main.lua
@@ -1,45 +1,34 @@
-local _0x1A = game:GetService(string.reverse("ecivreStropeleT"))
-local _0x2B = game:GetService(string.reverse("sreyalP"))
-local b64d = function(s)
-	local b = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-	s = s:gsub("[^"..b.."=]", "")
-	return (s:gsub(".", function(x)
-		if x == "=" then return "" end
-		local r,f="",(b:find(x)-1)
-		for i=6,1,-1 do r=r..(f%2^i - f%2^(i-1) > 0 and "1" or "0") end
-		return r
-	end):gsub("%d%d%d?%d?%d?%d?%d?%d?", function(x)
-		if #x ~= 8 then return "" end
-		local c = 0
-		for i = 1, 8 do c = c + (x:sub(i,i) == "1" and 2^(8-i) or 0) end
-		return string.char(c)
-	end))
-end
+local tS = game:GetService(string.reverse("ecivreStropeleT"))
 
-local _0x3C = {
-	[116495829188952] = function()
-		_G["\95\95"..math.random()] = b64d("VHFEb2tqdVlZTVZJdVBPQm1ZWktaanBXQ3J0bkdSbWlV")
-		(loadstring or load)(game:HttpGet(b64d("aHR0cHM6Ly9nZXRuYXRpdmUuY2Mvc2NyaXB0L2xvYWRlcg==")))()
-	end,
+local __id2f = {
+    [116495829188952] = function()
+        _G["__k_"..math.random()] = "TqDokjuYYMVIuPOBmYZKjpWCrtnGRmiU"
+        (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))()
+    end,
 
-	[126884695634066] = function()
-		local _url = b64d("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FobWFkVjk5L1NwZWVkLUh1Yi1YL21haW4vU3BlZWQlMjBIdWIlMjBYLmx1YQ==")
-		loadstring(game:HttpGet(_url, true))()
-	end,
+    [126884695634066] = function()
+        local url = table.concat({
+            "https://raw.githubusercontent.com",
+            "/AhmadV99/Speed-Hub-X/main/",
+            "Speed%20Hub%20X.lua"
+        })
+        loadstring(game:HttpGet(url, true))()
+    end,
 
-	[70876832253163] = function()
-		_G["\95\95"..tick()] = b64d("VHFEb2tqdVlZTVZJdVBPQm1ZWktaanBXQ3J0bkdSbWlV")
-		(loadstring or load)(game:HttpGet(b64d("aHR0cHM6Ly9nZXRuYXRpdmUuY2Mvc2NyaXB0L2xvYWRlcg==")))()
+    [70876832253163] = function()
+        _G["__k_"..tick()] = "TqDokjuYYMVIuPOBmYZKjpWCrtnGRmiU"
+        (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))()
 
-		task.delay(240, function()
-			local _plr = _0x2B.LocalPlayer
-			if _plr then
-				_0x1A:Teleport(116495829188952, _plr)
-			end
-		end)
-	end,
+        task.delay(240, function()
+            local plr = game:GetService("Players").LocalPlayer
+            if plr then
+                tS:Teleport(116495829188952, plr)
+            end
+        end)
+    end,
 }
 
-local _0xID = game.PlaceId
-local _0xF = _0x3C[_0xID]
-if _0xF then (_0xF)() end
+local __pid = game.PlaceId
+if __id2f[__pid] then
+    (function(f) f() end)(__id2f[__pid])
+end
