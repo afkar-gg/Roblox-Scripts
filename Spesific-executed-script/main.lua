1263283193cc450cd0c6eb7b50eab8117b722940
afkar-gg <afkargantengbetjir@gmail.com>
2025-07-11 21:02:54 +0800

    Update main.lua

--- Spesific-executed-script/main.lua
+++ Spesific-executed-script/main.lua
@@ -3,16 +3,19 @@ local TeleportService = game:GetService("TeleportService")
 local placeScripts = {
 [116495829188952] = function()
     script_key = "TqDokjuYYMVIuPOBmYZKjpWCrtnGRmiU"
-    (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))()
+    (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))();
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/test/refs/heads/main/deadrails.lua"))();
 end,
 
 [126884695634066] = function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()  
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))();
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/test/refs/heads/main/gag.lua"))();
 end,  
 
 [70876832253163] = function()  
     script_key = "TqDokjuYYMVIuPOBmYZKjpWCrtnGRmiU"  
-    (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))()  
+    (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))();
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/test/refs/heads/main/deadrails.lua"))();
 
     -- ⏳ Wait 2 minutes  
     task.delay(240, function()  
