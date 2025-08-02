0dd75fd4676916dbde53165d2299820c8d515f2c
afkar-gg <afkargantengbetjir@gmail.com>
2025-08-02 17:07:50 +0700

    Update main.lua

--- (Remastered)-Roblox-Joki-Proxy/main.lua
+++ (Remastered)-Roblox-Joki-Proxy/main.lua
@@ -9,9 +9,9 @@ local placeScripts = {
 end,
 
 [126884695634066] = function()
- loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/test/refs/heads/main/gag.lua"))();
-task.wait(5)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))();
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/test/refs/heads/main/gag.lua"))();
+    task.wait(5)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))();
 end,  
 
 [70876832253163] = function()  
