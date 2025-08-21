456c0c47e49af824ae09babea15b0255d34691b6
afkar-gg <afkargantengbetjir@gmail.com>
2025-08-21 20:12:03 +0700

    Update main.lua

--- (Remastered)-Roblox-Joki-Proxy/main.lua
+++ (Remastered)-Roblox-Joki-Proxy/main.lua
@@ -2,18 +2,35 @@ if not game:IsLoaded() then game.Loaded:Wait() end
 local TeleportService = game:GetService("TeleportService")
 
 local placeScripts = {
+-- Dead Rails (Lobby)
 [116495829188952] = function()
     script_key = "TqDokjuYYMVIuPOBmYZKjpWCrtnGRmiU"
     (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))();
     loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/test/refs/heads/main/deadrails.lua"))();
 end,
 
+-- 99 Nights In The Forest (Lobby)
+[79546208627805] = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/test/refs/heads/main/99natf.lua"))();
+    task.wait(5)
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9a77cef9455a36ab545e0b157878e42d.lua"))()
+end,
+
+-- 99 Nights In The Forest (Game)
+[126509999114328] = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/test/refs/heads/main/99natf.lua"))();
+    task.wait(5)
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9a77cef9455a36ab545e0b157878e42d.lua"))
+end,
+
+-- Grow a Garden
 [126884695634066] = function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/test/refs/heads/main/gag.lua"))();
     task.wait(5)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))();
 end,  
 
+-- Dead Rails (Game)
 [70876832253163] = function()  
     script_key = "TqDokjuYYMVIuPOBmYZKjpWCrtnGRmiU"
     task.wait(5);
@@ -28,18 +45,6 @@ end,
             TeleportService:Teleport(116495829188952, player)  
         end  
     end)  
-end,
-
-[79546208627805] = function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/test/refs/heads/main/99natf.lua"))();
-    task.wait(5)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))();
-end,
-
-[126509999114328] = function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/test/refs/heads/main/99natf.lua"))();
-    task.wait(5)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))();
 end,
 
 }
