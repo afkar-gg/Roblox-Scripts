41a2c6f3c255a83a1539fc05975bd57c03804841
afkar-gg <afkargantengbetjir@gmail.com>
2025-08-23 18:16:01 +0700

    Update main.lua

--- (Remastered)-Roblox-Joki-Proxy/main.lua
+++ (Remastered)-Roblox-Joki-Proxy/main.lua
@@ -26,17 +26,14 @@ end,
 -- 99 Nights In The Forest (Lobby)
 [79546208627805] = function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/test/refs/heads/main/99natf.lua"))();
-    task.wait(1)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/r4mpage4/LuaCom/refs/heads/main/saint.noob"))();
+    task.wait(30)
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/MQPS7/99-Night-in-the-Forset/refs/heads/main/Gfarm'))()
 
 end,
 
 -- 99 Nights In The Forest (Game)
 [126509999114328] = function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/test/refs/heads/main/99natf.lua"))();
-    task.wait(1)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/r4mpage4/LuaCom/refs/heads/main/saint.noob"))();
-
 end,
 
 -- Grow a Garden
