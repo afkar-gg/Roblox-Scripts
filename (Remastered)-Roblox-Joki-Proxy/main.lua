0ed465bcf57058d77b2d4b5581532ec3b7949357
afkar-gg <afkargantengbetjir@gmail.com>
2025-08-30 21:10:55 +0700

    Update main.lua

--- (Remastered)-Roblox-Joki-Proxy/main.lua
+++ (Remastered)-Roblox-Joki-Proxy/main.lua
@@ -35,7 +35,6 @@ end,
 -- 99 Nights In The Forest (Game)
 [126509999114328] = function()
     getgenv().WebhookURL = "https://discord.com/api/webhooks/1390221900734533734/-zZqj8OROemNZ9iDolBbnxUdmXCAdKU8pF5x6ncwSBxzdKBWTI9HXRIil3EGVxaIAE6_"
-    getgenv().AutoFarm = true
     loadstring(game:HttpGet("https://raw.githubusercontent.com/caomod2077/Script/refs/heads/main/Farm%20Diamond%20v2.lua"))();
     loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/test/refs/heads/main/99natf.lua"))();
     game:GetService("StarterGui"):SetCore("SendNotification", {
